e67334cac8b2b10f642583ba8062b3fcf3c329ab
MatkoUSKO <pialamar@gmail.com>
2019-04-02 20:51:47 +0200

    remove implementations of players

--- Isolation/MinimaxPlayer.py
+++ Isolation/MinimaxPlayer.py
@@ -1,50 +1,8 @@
 import random
 from game import get_opposite
 
-def eval(board, maximising_player):
-    return len(board.get_valid_moves(maximising_player))
-
 def minimax(board, depth, max_depth, maximising_player, minimising_player, player_on_turn):
-
-    # simulation ends here
-    if depth >= max_depth or board.is_finished(player_on_turn):
-        return eval(board, maximising_player), None
-
-    # we are the maximising player
-    if maximising_player == player_on_turn:
-        moves = board.get_valid_moves(player_on_turn)
-        value = -1000000
-        move = None
-
-        for minimax_move in moves:
-            copy_board = board.copy()
-            copy_board.apply_move(minimax_move, player_on_turn)
-
-            minimax_value, _ = minimax(copy_board, depth+1, max_depth, maximising_player, minimising_player, minimising_player)
-
-            if minimax_value > value:
-                value = minimax_value
-                move = minimax_move
-         
-        return value, move
-
-    # we are the minimising player
-    else: # minimising player
-        moves = board.get_valid_moves(player_on_turn)
-        value = 1000000
-        move = None
-
-        for minimax_move in moves:
-            copy_board = board.copy()
-            copy_board.apply_move(minimax_move, player_on_turn)
-
-            minimax_value, _ = minimax(copy_board, depth+1, max_depth, maximising_player, minimising_player, maximising_player)
-
-            if minimax_value < value:
-                value = minimax_value
-                move = minimax_move
-
-        return value, move
+    pass
 
 class Minimax_player:
     def __init__(self, side, max_depth):
--- Isolation/OnePlyPlayer.py
+++ Isolation/OnePlyPlayer.py
@@ -6,19 +6,5 @@ class One_ply_player:
         self.side = side
 
     def next_move(self, board):
-        moves = board.get_valid_moves(self.side)
-        min_move = None
-        min_moves_available = 10
-        
-        for move in moves:
-            copy_board = board.copy()
-            copy_board.apply_move(move, self.side)
-            opposite_player = get_opposite(board.left, board.right, self.side)
-            moves_available = len(copy_board.get_valid_moves(opposite_player))
-
-            if moves_available < min_moves_available:
-                min_moves_available = moves_available
-                min_move = move
-
-        return min_move
+        pass
 
